4100bc68cec65d35f9f176c2df7c3ff19446629b
Kacper Syrnik <kacper.syrnik1@gmail.com>
2022-12-29 23:32:12 +0100

    Add DataSource configuration

---
+++ hotel-booking-api/src/main/java/com/syrnik/hotelbookingapi/config/DataSourceConfiguration.java
@@ -0,0 +1,25 @@
+package com.syrnik.hotelbookingapi.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataSourceConfiguration {
+    private final Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url(environment.getProperty("spring.datasource.url"))
+                .username(environment.getProperty("spring.datasource.username"))
+                .password(environment.getProperty("spring.datasource.password"))
+                .driverClassName(environment.getProperty("spring.datasource.driver-class-name"))
+                .build();
+    }
+}
