98d358a111110555f44e59e6c70f156f367b680b
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-01-16 23:48:05 +0100

    Add built frontend to static files in spring boot app

--- hotel-booking-frontend/src/app/components/admin/country/admin-add-country/admin-add-country.component.ts
+++ hotel-booking-frontend/src/app/components/admin/country/admin-add-country/admin-add-country.component.ts
@@ -24,7 +24,7 @@ export class AdminAddCountryComponent {
 
     if (!this.isAddMode) {
       this.findCountryById(this.id).subscribe(country => this.form.patchValue(country));
-  }
+    }
   }
 
   onSubmit() {
